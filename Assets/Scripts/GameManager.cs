f64260cd8e3d30e2df2e5dfc18a9a8cfa10cb997
RubenRosoHui <59492671+RubenRosoHui@users.noreply.github.com>
2021-04-15 23:34:58 -0400

    Materials,Scripts, and Scenes
    The important parts of the game. The code, the visual background, and the individual items.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    bool gameEnded = false;
+    public float Delay = 1f;
+    public GameObject completeLevelUI;
+
+    public void CompleteLevel()
+    {
+        Debug.Log("End Level");
+        completeLevelUI.SetActive(true);
+    }
+
+    public void EndGame() 
+    {
+        if (gameEnded == false)
+        {
+            gameEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", Delay);
+            
+        }
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+}
